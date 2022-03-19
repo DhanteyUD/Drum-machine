ef95c1b3fd8691beaa03e0a0757aca7125c097b4
DhanteyUD <otseclinton60@gmail.com>
2022-03-19 11:11:56 +0100

    Pad Bank

---
+++ drum-machine/src/components/padBank.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import DrumPad from './drumPad';
+
+class PadBank extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let padBank;
+    if (this.props.power) {
+      padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+        return (
+          <DrumPad
+            clip={padBankArr[i].url}
+            clipId={padBankArr[i].id}
+            keyCode={padBankArr[i].keyCode}
+            keyTrigger={padBankArr[i].keyTrigger}
+            power={this.props.power}
+            updateDisplay={this.props.updateDisplay}
+          />
+        );
+      });
+    } else {
+      padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+        return (
+          <DrumPad
+            clip="#"
+            clipId={padBankArr[i].id}
+            keyCode={padBankArr[i].keyCode}
+            keyTrigger={padBankArr[i].keyTrigger}
+            power={this.props.power}
+            updateDisplay={this.props.updateDisplay}
+          />
+        );
+      });
+    }
+    return <div className="pad-bank">{padBank}</div>;
+  }
+}
+
+export default PadBank;
