3100e9906275541069f507c79478bed325a5211f
DhanteyUD <otseclinton60@gmail.com>
2022-06-11 10:54:05 +0100

    Updated Power Display

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      power: true,
+      power: false,
       display: String.fromCharCode(160),
       currentPadBank: bankOne,
       currentPadBankId: 'Heater Kit',
